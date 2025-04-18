1437bfff584f49ed24d8e7171e5e74f5e9479bbb
Stanislav Petr <stanislav.petr7@gmail.com>
2025-04-18 14:20:54 +0200

    adding models

---
+++ Models/TripPlace.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TravelTracker.Models
+{
+    public class TripPlace
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required]
+        public string Name { get; set; } = null!;
+        [Required]
+        public double Latitude { get; set; }
+        [Required]
+
+        public double Longitude { get; set; }
+        public string? Description { get; set; }
+
+        [ForeignKey("User")]
+        public Guid UserId { get; set; }
+
+        public User User { get; set; } = null!;
+    }
+}
