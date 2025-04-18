dde5455eb10fbe507590904150fec0e0fd6d5e9f
Stanislav Petr <stanislav.petr7@gmail.com>
2025-04-18 14:20:47 +0200

    map controller

---
+++ Controllers/MapController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TravelTracker.Data;
+using TravelTracker.Models;
+
+namespace TravelTracker.Controllers
+{
+    public class MapController: Controller
+    {
+        private readonly AppDbContext _context;
+        private readonly IConfiguration _config;
+
+        public MapController(AppDbContext context, IConfiguration config)
+        {
+            _context = context;
+            _config = config;
+        }
+
+        public IActionResult Index()
+        {
+            List<TripPlace> tripPlaces = _context.TripPlaces.Include(t => t.User).ToList();
+
+            ViewBag.GoogleMapsApiKey = _config["GoogleMaps:Apikey"];
+
+            return View(tripPlaces);
+        }
+    }
+}
---
+++ Controllers/UserController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TravelTracker.Data;
+
+namespace TravelTracker.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly AppDbContext _context;
+        
+        UserController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+
+    }
+}
