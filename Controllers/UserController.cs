b7dfdeac1221dc4b893e4437b07a13400af639d4
Stanislav Petr <stanislav.petr7@gmail.com>
2025-05-20 22:46:03 +0200

    adding controllers for authentication

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -2,6 +2,9 @@
 using Microsoft.AspNetCore.Mvc;
 using TravelTracker.Data;
 using TravelTracker.Models.DTOs;
+using Microsoft.AspNetCore.Identity;
+using TravelTracker.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace TravelTracker.Controllers
 {
@@ -17,19 +20,57 @@ namespace TravelTracker.Controllers
         public IActionResult Login() => View();
 
         [HttpPost]
-        public IActionResult Login(LoginDto loginDto)
+        public async Task<IActionResult> Login(LoginDto loginDto)
         {
-            
+            var foundUser = await _context.Users.FirstOrDefaultAsync(user => user.Email == loginDto.Email);
 
-            return RedirectToAction("Index", "Home");
+            if (foundUser == null)
+            {
+                return StatusCode(500, "User does not exist.");
+            }
+
+            var hasher = new PasswordHasher<object>();
+            var passwordsMatchResult = hasher.VerifyHashedPassword(null, foundUser.PasswordHash, loginDto.Password);
+
+
+            if (passwordsMatchResult == PasswordVerificationResult.Failed)
+            {
+                return StatusCode(500, "Authentication failed.");
+            }
+
+            return RedirectToAction("Index", "Map");
         }
 
         public IActionResult Register() => View();
 
         [HttpPost]
-        public IActionResult Register (RegisterDto registerDto)
+        public async Task<IActionResult> Register(RegisterDto registerDto)
         {
-            return RedirectToAction("Index", "Home");
+            var foundUser = await _context.Users.FirstOrDefaultAsync(user => user.Email == registerDto.Email);
+
+            if (foundUser != null)
+            {
+                return BadRequest(new {error = "User already exists" });
+            }
+
+            if (registerDto.Password != registerDto.RepeatPassword)
+            {
+                return BadRequest(new { error = "Passwords don't match"});
+            }
+
+            var hasher = new PasswordHasher<object>();
+            string hashedPassword = hasher.HashPassword(null, registerDto.Password);
+
+            var newUser = new User
+            {
+                Email = registerDto.Email,
+                PasswordHash = hashedPassword
+            };
+
+            await _context.Users.AddAsync(newUser);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction("Index", "Map");
         }
     }
 }
